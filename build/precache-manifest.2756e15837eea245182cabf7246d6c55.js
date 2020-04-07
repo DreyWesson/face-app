d09c9aaa8a751d2e00e822c05443107736de9519
dreyWesson <dreyWesson@gmail.com>
2020-04-07 09:58:03 +0100

    particle.js giving me uncaught error: super expression

---
+++ build/precache-manifest.2756e15837eea245182cabf7246d6c55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f9b88aae4a2595f5ad4",
+    "url": "/static/css/main.21c50295.chunk.css"
+  },
+  {
+    "revision": "4f9b88aae4a2595f5ad4",
+    "url": "/static/js/main.4f9b88aa.chunk.js"
+  },
+  {
+    "revision": "3e7672d55b3fea47d665",
+    "url": "/static/css/1.b52bd4ac.chunk.css"
+  },
+  {
+    "revision": "3e7672d55b3fea47d665",
+    "url": "/static/js/1.3e7672d5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7302d0a51ad745b26429bd2dcf23495a",
+    "url": "/static/media/brain-icon.7302d0a5.svg"
+  },
+  {
+    "revision": "67c52d87ec3f3c82af8bbd864fb13abd",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.beb28986e8d22846beb25ac0c31e7cb1.js"
+  "/precache-manifest.2756e15837eea245182cabf7246d6c55.js"
 );
 
 workbox.clientsClaim();
